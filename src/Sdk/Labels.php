4ca34083f27f9dc50ef8526436daa2b6e5d19005
Jaggy <jaggy@hey.com>
2024-03-08 01:39:46 +0800

    Add an API to only fetch workspace-specific labels

--- src/Sdk/Labels.php
+++ src/Sdk/Labels.php
@@ -38,6 +38,34 @@ class Labels extends Client
         }
     }
 
+    public function getWorkspaceLabels(): Dto\Labels
+    {
+        $query = "
+            query Labels {
+                issueLabels(filter: {
+                    team: { null: null }
+                }) {
+                    nodes {
+                        id
+                        name
+                        isGroup
+                    }
+                }
+            }
+        ";
+
+        $labelsArr = $this->cache->get('labels', function (ItemInterface $item) use ($query) {
+            $response = $this->http->post('/', ['query' => $query]);
+            return $this->process($response);
+        });
+
+        try {
+            return Mapper::get()->map(Dto\Labels::class, Source::array($labelsArr['issueLabels']));
+        } catch (MappingError $error) {
+            throw new Exception('Teams DTO Mapping error:' . $error->getMessage());
+        }
+    }
+
     public function getOne(string $id): Dto\Label
     {
         $query = "
